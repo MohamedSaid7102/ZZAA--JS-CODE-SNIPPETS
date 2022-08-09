03518cd946fb141af69a0ba6320ca33e7be4bd04
Shlef <mohamedsaidwork@gmail.com>
2022-08-09 06:39:13 +0200

    Implemented Throttling Logic

--- debouncing-and-throttling/debounce_and_throttle.js
+++ debouncing-and-throttling/debounce_and_throttle.js
@@ -16,12 +16,45 @@ const debounce = (cb, delay = 1000) => {
 
 const updateDebounceText = debounce((text) => {
   debounceText.textContent = text;
-},500);
+}, 500);
 // Throttle
+const throttle = (cb, delay = 1000) => {
+  let shouldWait = false; /**By default you shouldn't wait, you're just here..! */
+  let waitingArgs = null;
 
+  /**We put this into a function to make possible to reset the waiting timer */
+  const timeoutFunc = () => {
+    /**If there is no waiting arguments 'User didn't type anything during the delay */
+    if (waitingArgs === null) {
+      shouldWait = false;
+    } else {
+      cb(...waitingArgs);
+      waitingArgs = null; /**Reset waiting arguments */
+      setTimeout(timeoutFunc, delay); /**Reset the timeout timer */
+    }
+  };
+
+  return (...args) => {
+    if (shouldWait) {
+      waitingArgs = args;
+      return;
+    }
+    // Immediatelly call that function
+    cb(...args);
+    shouldWait = true;
+
+    // Then wait a 'delay' period to call it again
+    setTimeout(timeoutFunc, delay);
+  };
+};
+
+const updateThrottleText = throttle((text) => {
+  throttleText.textContent = text;
+});
 // Invocation
 input.addEventListener('input', (e) => {
   // Noraml
   defaultText.textContent = e.target.value;
   updateDebounceText(e.target.value);
+  updateThrottleText(e.target.value);
 });
